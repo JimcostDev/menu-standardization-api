5dda910145b47be9e233f543cf53a5cf12dc2241
JimcostDev <rjak727@gmail.com>
2023-12-20 15:19:46 +0100

    add .ico

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from api import router as api_router
 
 app = FastAPI()
+
+# Agregar la ruta para servir archivos estáticos, incluido el favicon
+app.mount("/static", StaticFiles(directory="assets"), name="static")
+
+# Incluir las rutas de tu API
 app.include_router(api_router)
 
 if __name__ == "__main__":
