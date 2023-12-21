bb3c5635c7d7a1c50048cc7e7f8b2fa096f87f4c
JimcostDev <rjak727@gmail.com>
2023-12-21 13:03:53 +0100

    mejoramos la documetación en api y models

--- app/api.py
+++ app/api.py
@@ -7,22 +7,42 @@ from utils import is_valid_object_id
 router = APIRouter()
 
 # Consultar las categorías y sus productos
-@router.get("/categories/", response_model=List[Category])
+@router.get("/categories/", response_model=List[Category],
+            summary="Obtener todas las categorías",
+            description="Este endpoint devuelve una lista de todas las categorías disponibles.")
 def read_categories():
     return database.get_categories()
 
 
-@router.get("/categories/{category_name}/products/", response_model=List[Product])
+@router.get("/categories/{category_name}/products/", response_model=List[Product],
+            summary="Obtener productos por categoría",
+            description="Devuelve todos los productos pertenecientes a una categoría específica. "
+                        "La categoría se especifica por su nombre.")
 def read_category_products(category_name: str):
     return database.get_category_products(category_name)
 
 # Consultar los productos
-@router.get("/products/", response_model=List[Product])
+@router.get(
+    "/products/", 
+    response_model=List[Product],
+    summary="Obtener todos los productos",
+    description="Este endpoint devuelve una lista de todos los productos disponibles en la base de datos. "
+                "Cada producto incluye detalles como nombre, descripción, categoría, etiquetas y precio. "
+                "Es útil para obtener una vista general de todos los productos ofrecidos."
+)
 def read_products():
     return database.get_products()
 
 # Consultar por id de producto
-@router.get("/products/{product_id}", response_model=Product)
+@router.get(
+    "/products/{product_id}",
+    response_model=Product,
+    summary="Consultar producto por ID",
+    description="Este endpoint permite obtener la información detallada de un producto específico, "
+                "identificado por su ID. Si el ID del producto es válido y el producto existe en la base de datos, "
+                "retorna todos los detalles del producto, incluyendo nombre, descripción, categoría, etiquetas y precio. "
+                "En caso de que el ID no sea válido o el producto no exista, se retornará un error."
+)
 def read_product(product_id: str = Path(..., title="The ID of the product to get")):
     if not is_valid_object_id(product_id):
         raise HTTPException(status_code=400, detail="Invalid product ID")
@@ -33,7 +53,15 @@ def read_product(product_id: str = Path(..., title="The ID of the product to get
     return product
 
 # Consultar por id de categoría
-@router.get("/categories/{category_id}", response_model=Category)
+@router.get(
+    "/categories/{category_id}", 
+    response_model=Category,
+    summary="Consultar categoría por ID",
+    description="Este endpoint permite obtener la información detallada de una categoría específica, "
+                "identificada por su ID. Si el ID de la categoría es válido y la categoría existe en la base de datos, "
+                "retorna todos los detalles de la categoría, como su nombre. "
+                "En caso de que el ID no sea válido o la categoría no exista, se retornará un error."
+)
 def read_category(category_id: str = Path(..., title="The ID of the category to get")):
     if not is_valid_object_id(category_id):
         raise HTTPException(status_code=400, detail="Invalid category ID")
@@ -45,19 +73,39 @@ def read_category(category_id: str = Path(..., title="The ID of the category to 
 
 
 # Crear una categoría
-@router.post("/categories/")
+@router.post(
+    "/categories/",
+    summary="Crear una nueva categoría",
+    description="Este endpoint permite crear una nueva categoría en la base de datos. "
+                "Se debe proporcionar la información de la categoría en el cuerpo de la solicitud. "
+                "Tras la creación exitosa, retorna un mensaje confirmando que la categoría ha sido creada."
+)
 def create_category(category: Category):
     database.create_category(category)
     return {"message": "Category created"}
 
 # Crear un producto
-@router.post("/products/")
+@router.post(
+    "/products/",
+    summary="Crear un nuevo producto",
+    description="Este endpoint permite crear un nuevo producto en la base de datos. "
+                "Los detalles del producto, como nombre, descripción, categoría, etiquetas y precio, "
+                "deben ser proporcionados en el cuerpo de la solicitud. "
+                "Después de crear el producto con éxito, se devuelve un mensaje confirmando la creación."
+)
 def create_product(product: Product):
     database.create_product(product)
     return {"message": "Product created"}
 
 # Eliminar un producto
-@router.delete("/products/{product_id}")
+@router.delete(
+    "/products/{product_id}",
+    summary="Eliminar un producto",
+    description="Este endpoint elimina un producto específico de la base de datos, identificado por su ID. "
+                "El ID del producto debe ser proporcionado en la URL. Si el producto con el ID especificado existe, "
+                "será eliminado y se retornará un mensaje de confirmación. "
+                "Si el ID no es válido o el producto no se encuentra, se retornará un error."
+)
 def delete_product(product_id: str):
     if not is_valid_object_id(product_id):
         raise HTTPException(status_code=400, detail="Invalid product ID")
@@ -68,7 +116,14 @@ def delete_product(product_id: str):
     raise HTTPException(status_code=404, detail="Product not found")
 
 # Eliminar una categoría
-@router.delete("/categories/{category_id}")
+@router.delete(
+    "/categories/{category_id}",
+    summary="Eliminar una categoría",
+    description="Este endpoint elimina una categoría específica de la base de datos, identificada por su ID. "
+                "El ID de la categoría debe ser proporcionado en la URL. Si la categoría con el ID especificado existe, "
+                "será eliminada y se retornará un mensaje de confirmación. "
+                "Si el ID no es válido o la categoría no se encuentra, se retornará un error."
+)
 def delete_category(category_id: str):
     if not is_valid_object_id(category_id):
         raise HTTPException(status_code=400, detail="Invalid category ID")
@@ -80,7 +135,15 @@ def delete_category(category_id: str):
 
 
 # Actualizar una categoría
-@router.put("/categories/{category_id}")
+@router.put(
+    "/categories/{category_id}",
+    summary="Actualizar una categoría",
+    description="Este endpoint actualiza los detalles de una categoría existente en la base de datos, "
+                "identificada por su ID. El ID de la categoría debe ser proporcionado en la URL y los nuevos "
+                "detalles de la categoría en el cuerpo de la solicitud. Si la actualización es exitosa, retorna "
+                "un mensaje de confirmación. En caso de que el ID no sea válido o la categoría no se encuentre, "
+                "se retornará un error."
+)
 def update_category(category_id: str, category: Category):
     if not is_valid_object_id(category_id):
         raise HTTPException(status_code=400, detail="Invalid category ID")
@@ -91,7 +154,15 @@ def update_category(category_id: str, category: Category):
     raise HTTPException(status_code=404, detail="Category not found")
 
 # Actualizar un producto
-@router.put("/products/{product_id}")
+@router.put(
+    "/products/{product_id}",
+    summary="Actualizar un producto",
+    description="Este endpoint actualiza los detalles de un producto existente en la base de datos, "
+                "identificado por su ID. El ID del producto debe ser proporcionado en la URL y los nuevos "
+                "detalles del producto en el cuerpo de la solicitud. Si la actualización es exitosa, retorna "
+                "un mensaje de confirmación. En caso de que el ID no sea válido o el producto no se encuentre, "
+                "se retornará un error."
+)
 def update_product(product_id: str, product: Product):
     if not is_valid_object_id(product_id):
         raise HTTPException(status_code=400, detail="Invalid product ID")
--- app/models.py
+++ app/models.py
@@ -1,12 +1,37 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, constr, validator
 from typing import List
 
 class Product(BaseModel):
-    name: str
-    description: str
-    category: str
-    tags: List[str]
-    price: float
+    name: constr(min_length=1, max_length=100) = Field(..., description="El nombre del producto.")
+    description: constr(min_length=1, max_length=300) = Field(..., description="Una breve descripción del producto.")
+    category: constr(min_length=1, max_length=100) = Field(..., description="La categoría a la que pertenece el producto.")
+    tags: List[str] = Field(..., description="Etiquetas asociadas al producto, como 'vegano', 'sin gluten', etc.")
+    price: float = Field(..., gt=0, description="El precio del producto, debe ser mayor que 0.")
+
+    @validator('tags')
+    def validate_tags(cls, v):
+        if len(v) < 1 or len(v) > 10:
+            raise ValueError('Debe haber entre 1 y 10 etiquetas')
+        return v
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Torta de Chocolate",
+                "description": "Una deliciosa torta de chocolate con capas de crema y cerezas.",
+                "category": "Postres",
+                "tags": ["dulce", "chocolate", "sin gluten"],
+                "price": 19.99
+            }
+        }
+
 
 class Category(BaseModel):
-    name: str
+    name: constr(min_length=1, max_length=100) = Field(..., description="El nombre de la categoría.")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Postres"
+            }
+        }
