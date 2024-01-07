56c34877b9f76aa65c82af078ab5e5163af6a624
JimcostDev <rjak727@gmail.com>
2024-01-07 16:15:33 +0100

    mapeo del modelo user

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel, Field, constr, validator
+from pydantic import BaseModel, Field, constr, validator, EmailStr, HttpUrl
 from typing import List, Optional
 
+# ------------------ PRODUCT ----------------
 class ProductBase(BaseModel):
     name: constr(min_length=1, max_length=100) = Field(..., description="El nombre del producto.")
     description: constr(min_length=1, max_length=300) = Field(..., description="Una breve descripción del producto.")
@@ -16,7 +17,7 @@ class ProductBase(BaseModel):
         return v   
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 class ProductCreate(ProductBase):
     pass
@@ -25,7 +26,7 @@ class Product(ProductBase):
     id: Optional[str] = Field(None, description="ID del producto")
 
     class Config:
-        schema_extra = {
+        json_schema_extra = {
             "example": {
                 "name": "Torta de Chocolate",
                 "description": "Una deliciosa torta de chocolate con capas de crema y cerezas.",
@@ -36,15 +37,59 @@ class Product(ProductBase):
             }
         }
 
+# ------------------ CATEGORY ----------------
 class Category(BaseModel):
     name: constr(min_length=1, max_length=100) = Field(..., description="El nombre de la categoría.")
     image: str = Field(..., description="URL de la imagen de la categoría")
     id: Optional[str] = Field(None, description="ID de la categoría")
 
     class Config:
-        schema_extra = {
+        json_schema_extra = {
             "example": {
                 "name": "Postres",
                 "image": "https://example.com/postres.jpg"
             }
         }
+
+
+# ------------------ USER ----------------
+class GoogleInfo(BaseModel):
+    google_id: str = Field(..., description="ID proporcionado por Google")
+    access_token: str = Field(..., description="Token de acceso de Google")
+    refresh_token: str = Field(..., description="Token de actualización de Google")
+    # Otros campos relevantes proporcionados por Google
+
+class UserBase(BaseModel):
+    """Base schema for User model."""
+    username: constr(min_length=4, max_length=50) = Field(..., description="Nombre de usuario (entre 4 y 50 caracteres)")
+
+class UserCreate(BaseModel):
+    """Schema for creating a User."""
+    email: EmailStr = Field(..., description="Dirección de correo electrónico")
+    password: constr(min_length=8)
+    avatar: HttpUrl = Field(..., description="URL de la imagen o avatar del usuario")
+    
+    @validator('password')
+    def validate_password(cls, v):
+        if not any(c.isupper() for c in v):
+            raise ValueError('La contraseña debe contener al menos una letra mayúscula')
+        if not any(c.islower() for c in v):
+            raise ValueError('La contraseña debe contener al menos una letra minúscula')
+        if not any(c.isdigit() for c in v):
+            raise ValueError('La contraseña debe contener al menos un número')
+        if not any(c in '!@#$%^&*:' for c in v):
+            raise ValueError('La contraseña debe contener al menos un carácter especial')
+        return v
+
+class UserResponse(BaseModel):
+    """Schema for User response."""
+    username: constr(min_length=4, max_length=50) = Field(..., description="Nombre de usuario (entre 4 y 50 caracteres)")
+    email: EmailStr = Field(..., description="Dirección de correo electrónico")
+    avatar: HttpUrl = Field(..., description="URL del avatar del usuario")
+    roles: List[str] = Field(..., description="Lista de roles del usuario")
+    google_info: Optional[GoogleInfo] = Field(None, description="Información opcional proporcionada por Google")
+
+class UserInDB(UserResponse):
+    """Schema for User stored in database."""
+    created_at: str = Field(..., description="Fecha de creación del usuario")
+    updated_at: str = Field(..., description="Fecha de última actualización del usuario")
